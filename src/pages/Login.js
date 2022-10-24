b559b4770e54a4543d919cb448106e82e90908c5
Leonardo Prado <leonardo.rprado@gmail.com>
2022-10-24 21:50:47 +0100

    finalizado req 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import Input from '../components/Input';
 
 function Login() {
   return (
     <div>
       <h3>Login</h3>
+      <div>
+        <Input
+          name="email"
+          placeholder="Email"
+          type="email"
+          // value={ }
+          // onChange={}
+          dataTestid="email-input"
+        />
+        <Input
+          name="password"
+          placeholder="Senha"
+          type="password"
+          // value={ }
+          // onChange={ }
+          dataTestid="password-input"
+        />
+        <button
+          type="button"
+          data-testid="login-submit-btn"
+          // onClick={ }
+        >
+          Entrar
+        </button>
+      </div>
     </div>
   );
 }
