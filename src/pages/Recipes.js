1d3e283f4a5b857e29bbba5dacbef1141fe425b7
Leonardo Prado <leonardo.rprado@gmail.com>
2022-10-26 16:05:39 +0100

    informação ingrediente redirecionado na tela

--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import Context from '../context/Context';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
 
 function Recipes({ location: { pathname } }) {
+  const { resultsSearch } = useContext(Context);
+  console.log(resultsSearch);
   const verifyRouteMeals = pathname === '/meals';
-
   return (
     <div>
       <Header
@@ -17,6 +19,48 @@ function Recipes({ location: { pathname } }) {
         searchIcon={ searchIcon }
         search
       />
+      {
+        verifyRouteMeals
+          ? (
+            resultsSearch?.map((e, index) => (
+              <div
+                key={ e.strMeal }
+                data-testid={ `${index}-recipe-card` }
+              >
+                <h3
+                  data-testid={ `${index}-card-name` }
+                >
+                  {e.strMeal}
+                </h3>
+                <img
+                  src={ e.strMealThumb }
+                  alt={ e.strMeal }
+                  data-testid={ `${index}-card-img` }
+                />
+              </div>
+            ))
+          )
+          : (
+            resultsSearch?.map((e, index) => (
+              <div
+                key={ e.strDrink }
+                data-testid={ `${index}-recipe-card` }
+              >
+                <h3
+                  data-testid={ `${index}-card-name` }
+                >
+                  {e.strDrink}
+                </h3>
+                <img
+                  src={ e.strDrinkThumb }
+                  alt={ e.strDrink }
+                  data-testid={ `${index}-card-img` }
+                />
+              </div>
+            ))
+
+          )
+      }
     </div>
   );
 }
