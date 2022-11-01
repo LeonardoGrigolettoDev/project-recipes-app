ca0da41840e2ca10ab19019dc7ac36954f5296a7
Bruno Modesto <contato.obruniin@gmail.com>
2022-11-01 18:27:16 -0300

    concluido req38 :rocket:

--- src/pages/RecipesInProgress.js
+++ src/pages/RecipesInProgress.js
@@ -1,128 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import Checkbox from '../components/Checkbox';
-// import loadRecipe from '../services/fetchRecipeInProgress';
+import meals from '../components/MealTest';
+import drinks from '../components/DrinkTest';
 
 function RecipesInProgress() {
-  const [dataRecipe, setdataRecipe] = useState([]);
-  const meals = {
-    idMeal: '52771',
-    strMeal: 'Spicy Arrabiata Penne',
-    strDrinkAlternate: null,
-    strCategory: 'Vegetarian',
-    strArea: 'Italian',
-    strInstructions: 'Bring a large pot of water to a boil. Add kosher salt to the boiling water, then add the pasta. Cook according to the package instructions, about 9 minutes. In a large skillet over medium-high heat, add the olive oil and heat until the oil starts to shimmer. Add the garlic and cook, stirring, until fragrant, 1 to 2 minutes. Add the chopped tomatoes, red chile flakes, Italian seasoning and salt and pepper to taste. Bring to a boil and cook for 5 minutes. Remove from the heat and add the chopped basil. Drain the pasta and add it to the sauce. Garnish with Parmigiano-Reggiano flakes and more basil and serve warm.',
-    strMealThumb: 'https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg',
-    strTags: 'Pasta,Curry',
-    strYoutube: 'https://www.youtube.com/watch?v=1IszT_guI08',
-    strIngredient1: 'penne rigate',
-    strIngredient2: 'olive oil',
-    strIngredient3: 'garlic',
-    strIngredient4: 'chopped tomatoes',
-    strIngredient5: 'red chile flakes',
-    strIngredient6: 'italian seasoning',
-    strIngredient7: 'basil',
-    strIngredient8: 'Parmigiano-Reggiano',
-    strIngredient9: '',
-    strIngredient10: '',
-    strIngredient11: '',
-    strIngredient12: '',
-    strIngredient13: '',
-    strIngredient14: '',
-    strIngredient15: '',
-    strIngredient16: null,
-    strIngredient17: null,
-    strIngredient18: null,
-    strIngredient19: null,
-    strIngredient20: null,
-    strMeasure1: '1 pound',
-    strMeasure2: '1/4 cup',
-    strMeasure3: '3 cloves',
-    strMeasure4: '1 tin ',
-    strMeasure5: '1/2 teaspoon',
-    strMeasure6: '1/2 teaspoon',
-    strMeasure7: '6 leaves',
-    strMeasure8: 'spinkling',
-    strMeasure9: '',
-    strMeasure10: '',
-    strMeasure11: '',
-    strMeasure12: '',
-    strMeasure13: '',
-    strMeasure14: '',
-    strMeasure15: '',
-    strMeasure16: null,
-    strMeasure17: null,
-    strMeasure18: null,
-    strMeasure19: null,
-    strMeasure20: null,
-    strSource: null,
-    dateModified: null,
-  };
-
-  const drinks = {
-    idDrink: '178319',
-    strDrink: 'Aquamarine',
-    strDrinkAlternate: null,
-    strDrinkES: null,
-    strDrinkDE: null,
-    strDrinkFR: null,
-    'strDrinkZH-HANS': null,
-    'strDrinkZH-HANT': null,
-    strTags: null,
-    strVideo: null,
-    strCategory: 'Cocktail',
-    strIBA: null,
-    strAlcoholic: 'Alcoholic',
-    strGlass: 'Martini Glass',
-    strInstructions: 'Shake well in a shaker with ice. Strain in a martini glass.',
-    strInstructionsES: null,
-    strInstructionsDE: null,
-    strInstructionsFR: null,
-    'strInstructionsZH-HANS': null,
-    'strInstructionsZH-HANT': null,
-    strDrinkThumb: 'https://www.thecocktaildb.com/images/media/drink/zvsre31572902738.jpg',
-    strIngredient1: 'Hpnotiq',
-    strIngredient2: 'Pineapple Juice',
-    strIngredient3: 'Banana Liqueur',
-    strIngredient4: '',
-    strIngredient5: '',
-    strIngredient6: '',
-    strIngredient7: '',
-    strIngredient8: null,
-    strIngredient9: null,
-    strIngredient10: null,
-    strIngredient11: null,
-    strIngredient12: null,
-    strIngredient13: null,
-    strIngredient14: null,
-    strIngredient15: null,
-    strMeasure1: '2 oz',
-    strMeasure2: '1 oz',
-    strMeasure3: '1 oz',
-    strMeasure4: '',
-    strMeasure5: '',
-    strMeasure6: '',
-    strMeasure7: '',
-    strMeasure8: null,
-    strMeasure9: null,
-    strMeasure10: null,
-    strMeasure11: null,
-    strMeasure12: null,
-    strMeasure13: null,
-    strMeasure14: null,
-    strMeasure15: null,
-    strCreativeCommonsConfirmed: 'No',
-    dateModified: null,
-  };
+  const [dataMeals, setDataMeals] = useState([]);
+  const [dataDrinks, setDataDrinks] = useState([]);
 
   const history = useHistory();
   const { location: { pathname } } = history;
-  const mealOrDrink = pathname.includes('drink');
+  const mealOrDrink = pathname.includes('meals');
+  // console.log(mealOrDrink);
 
-  useEffect(() => { setdataRecipe(meals); }, []);
+  useEffect(() => { setDataMeals(meals); }, []);
+  useEffect(() => { setDataDrinks(drinks); }, []);
 
-  const getMeasureAndIngredient = (param) => {
-    const arrKeyAndValue = Object.entries(meals);
+  const getMeasureAndIngredient = (param, param2) => {
+    const arrKeyAndValue = Object.entries(param2);
     const arrFiltered = [];
     arrKeyAndValue.forEach((item) => {
       const verifyItems = item[0]
@@ -134,11 +29,22 @@ function RecipesInProgress() {
     });
     return arrFiltered;
   };
-
-  const arrMeasure = getMeasureAndIngredient('strMeasure');
-  const arrIngredient = getMeasureAndIngredient('strIngredient');
+  let arrMeasure;
+  let arrIngredient;
+  if (mealOrDrink) {
+    arrMeasure = getMeasureAndIngredient('strMeasure', dataMeals);
+    arrIngredient = getMeasureAndIngredient('strIngredient', dataMeals);
+  } else {
+    arrMeasure = getMeasureAndIngredient('strMeasure', dataDrinks);
+    arrIngredient = getMeasureAndIngredient('strIngredient', dataDrinks);
+  }
   const measureAndIngredient = arrMeasure
     .map((measure, index) => `${measure} - ${arrIngredient[index]}`);
+
+  console.log(mealOrDrink);
+  // getMeasureAndIngredient();
+  // const measureAndIngredient = arrMeasure
+  // .map((measure, index) => `${measure} - ${arrIngredient[index]}`);
 
   // const arrKeyAndValue2 = Object.entries(meals);
   // const objectToArray = [];
@@ -147,60 +53,118 @@ function RecipesInProgress() {
   //   return objectToArray;
   // });
   // console.log(objectToArray);
+  // console.log(dataDrinks);
   return (
-    <div>
-      <div>
-        <img
-          src={ dataRecipe.strMealThumb }
-          alt={ dataRecipe.strMeal }
-          data-testid="recipe-photo"
-        />
-        <h2
-          data-testid="recipe-title"
-        >
-          { dataRecipe.strMeal }
-        </h2>
-        <h4
-          data-testid="recipe-category"
-        >
-          { dataRecipe.strCategory }
-        </h4>
-        <ul>
-          {
-            measureAndIngredient.map((item, index) => (
-              <li key={ index }>
-                <Checkbox index={ index } texto={ item } />
-              </li>
 
-            ))
-          }
-        </ul>
-        <span
-          data-testid="instructions"
-        >
-          { dataRecipe.strInstructions }
-        </span>
+    <div>
+      { mealOrDrink ? (
         <div>
-          <button
-            type="button"
-            data-testid="share-btn"
+          <img
+            src={ dataMeals.strMealThumb }
+            alt={ dataMeals.strMeal }
+            data-testid="recipe-photo"
+          />
+          <h2
+            data-testid="recipe-title"
           >
-            Compartilhar
-          </button>
-          <button
-            type="button"
-            data-testid="favorite-btn"
+            { dataMeals.strMeal }
+          </h2>
+          <h4
+            data-testid="recipe-category"
           >
-            Favoritar
-          </button>
-          <button
-            type="button"
-            data-testid="finish-recipe-btn"
+            { dataMeals.strCategory }
+          </h4>
+          <ul>
+            {
+              measureAndIngredient.map((item, index) => (
+                <li key={ index }>
+                  <Checkbox index={ index } texto={ item } />
+                </li>
+
+              ))
+            }
+          </ul>
+          <span
+            data-testid="instructions"
           >
-            Finalizar
-          </button>
+            { dataMeals.strInstructions }
+          </span>
+          <div>
+            <button
+              type="button"
+              data-testid="share-btn"
+            >
+              Compartilhar
+            </button>
+            <button
+              type="button"
+              data-testid="favorite-btn"
+            >
+              Favoritar
+            </button>
+            <button
+              type="button"
+              data-testid="finish-recipe-btn"
+            >
+              Finalizar
+            </button>
+          </div>
         </div>
-      </div>
+      ) : (
+        <div>
+          <img
+            src={ dataDrinks.strDrinkThumb }
+            alt={ dataDrinks.strDrink }
+            data-testid="recipe-photo"
+          />
+          <h2
+            data-testid="recipe-title"
+          >
+            { dataDrinks.strDrink }
+          </h2>
+          <h4
+            data-testid="recipe-category"
+          >
+            { dataDrinks.strAlcoholic }
+          </h4>
+          <ul>
+            {
+              measureAndIngredient.map((item2, index2) => (
+                <li key={ index2 }>
+                  <Checkbox index={ index2 } texto={ item2 } />
+                </li>
+
+              ))
+            }
+          </ul>
+          <span
+            data-testid="instructions"
+          >
+            { dataDrinks.strInstructions }
+          </span>
+          <div>
+            <button
+              type="button"
+              data-testid="share-btn"
+            >
+              Compartilhar
+            </button>
+            <button
+              type="button"
+              data-testid="favorite-btn"
+            >
+              Favoritar
+            </button>
+            <button
+              type="button"
+              data-testid="finish-recipe-btn"
+            >
+              Finalizar
+            </button>
+          </div>
+        </div>
+      )}
+
     </div>
   );
 }
