5232de245ede554d72dc730207bb378d138d6ac2
Bruno Modesto <contato.obruniin@gmail.com>
2022-11-03 17:53:49 -0300

    rodando o push novamente

--- src/pages/RecipeInProgress.js
+++ src/pages/RecipeInProgress.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useContext } from 'react';
+import React, { useEffect, useContext } from 'react'; // teste
 import { useHistory } from 'react-router-dom';
 import Checkbox from '../components/Checkbox';
 import meals from '../components/MealTest';
